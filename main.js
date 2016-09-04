2e44c9cee992bf156538f0f1ccc282098f12a164
廖冠豪 <whatsupduckiambunny@gmail.com>
2016-09-04 11:53:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var canvus = document.getElementById("game-canvus");
 var ctx = canvus.getContext("2d");
-function(){
+var enemyImg = document.createElement("img");
+enemImg.src = "images/slime.gif"
+function draw(){
   //將背景圖片畫在canvus上的(0,0)位置
   ctx.drawImage(bgImg,0,0);
+  //
+  ctx.drawImage(enemyImg,0,0)
+  
 }
 //等待1000毫秒再執行draw函式
 setTimeoutdraw(draw,1000);
