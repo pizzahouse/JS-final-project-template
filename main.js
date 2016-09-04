99777ef03c9325f70f561c5bc116fb2dd1cba163
廖冠豪 <whatsupduckiambunny@gmail.com>
2016-09-04 11:40:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var bgImg = document.creatElement("img");
+var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var canvus = document.getElementById("game-canvus");
 var ctx = canvus.getContext("2d");
