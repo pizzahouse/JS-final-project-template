b76be393df93b671801d9a4afe6c38aee9f20a89
廖冠豪 <whatsupduckiambunny@gmail.com>
2016-09-04 11:35:53 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+var bgImg = document.creat.Element("img");
+bgImg.src = "images/map.png";
+var canvus = document.getElementById("game-canvus");
+var ctx = canvus.getContext("2d");
+function(){
+  //將背景圖片畫在canvus上的(0,0)位置
+  ctx.drawImage(bgImg,0,0);
+}
+//等待1000毫秒再執行draw函式
+setTimeoutdraw(draw,1000);
