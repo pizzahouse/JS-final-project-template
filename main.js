314a4a59db52db5d763174939535692a73abe514
廖冠豪 <whatsupduckiambunny@gmail.com>
2016-09-11 11:01:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 var slimeImg = document.createElement("img");
 slimeImg.src = "images/slime.gif"
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower-btn.png"
 function draw(){
   //將背景圖片畫在canvus上的(0,0)位置
   ctx.drawImage(bgImg,0,0);
   //
   ctx.drawImage(slimeImg,slime.x,slime.y)
+  //
+  ctx.drawImage(towerImg,574,416)
   }
 //等待16毫秒再執行draw函式(重複畫)
 setInterval(draw,16);
