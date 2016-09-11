eebc1ae2f09e05c95ac31bcca1a4186b5c3ecb5f
廖冠豪 <whatsupduckiambunny@gmail.com>
2016-09-11 12:23:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ tower_btn_Img.src = "images/tower-btn.png"
 var towerImg = document.createElement("img");
 towerImg.src = "images/tower.png"
 var cursor = {x:0,y:0}
+//防禦塔跟著滑鼠游標
+$("#game-canvas").on("mousemove",function(event){
+  cursor.x = event.offsetX;
+  cursor.y = event.offsetY;
+});
+var isbuilding = false;
+$("#game-canvas").on("click",function(){
+  if(cursor.x >= 640-664 && cursor.y >= 480-64){
+    console.log("click");
+  }
+})
 function draw(){
   //將背景圖片畫在canvus上的(0,0)位置
   ctx.drawImage(bgImg,0,0);
@@ -18,18 +29,15 @@ function draw(){
   ctx.drawImage(tower_btn_Img,640-64,480-64,64,64);
   //
   ctx.drawImage(towerImg,cursor.x,cursor.y);
-  }
+  //
+  
+}
 //等待16毫秒再執行draw函式(重複畫)
 setInterval(draw,16);
 var slime = {
   x:0,
   y:0
 };
-//
-$("#game-canvas").on("mousemove",function(event){
-  cursor.x = event.offsetX;
-  cursor.y = event.offsetY;
-});
 
 
 
