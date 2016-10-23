fe3ad6a87671eff5773bb530ef0ca4183652fb21
廖冠豪 <whatsupduckiambunny@gmail.com>
2016-10-23 12:36:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ var tower = {
       var distance = Math.sqrt(
         Math.pow(this.x - enemies[i].x,2) + Math.pow(this.y - enemies[i].y,2)
       );
-      if(distance <= this.rangee){
+      if(distance <= this.range){
         this.aimingEnemyId = i;
         return;
       }
